fbd243721784eca83ac231592f6c842965ab24e2
jiajunhui <309812983@qq.com>
2016-08-17 16:09:06 +0800

    update interface IExtendHandle

--- player-library/src/main/java/com/xapp/jjh/base_ijk/inter/IExtendHandle.java
+++ player-library/src/main/java/com/xapp/jjh/base_ijk/inter/IExtendHandle.java
@@ -7,7 +7,7 @@ import com.xapp.jjh.base_ijk.listener.OnSlideHandleListener;
  * Created by Taurus on 2016/8/10.
  * ------------------------------------
  */
-public interface IExtendHandle extends IVideoPlayer{
+public interface IExtendHandle{
     void setSlideChangeLightEnable(boolean enable);
     void setSlideChangeSoundEnable(boolean enable);
     void setSlideSeekEnable(boolean enable);
